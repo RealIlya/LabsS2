da787a5177939cc17aa9c007e2ead80b6baabe9b
RealIlya <hairais@mail.ru>
2024-04-02 17:18:08 +0700

    Terminal developing in lab_itiop4

--- lab_itiop4/src/commander/Terminal.cpp
+++ lab_itiop4/src/commander/Terminal.cpp
@@ -1,13 +1,52 @@
 #include "Terminal.hpp"
 
+Terminal::CommandText::CommandText(char shortOption, std::string longOption, std::string text)
+{
+    this->shortOption = shortOption;
+    this->longOption = longOption;
+    this->text = text;
+}
+
+bool Terminal::CommandText::compare(std::string option)
+{
+    return this->longOption == option || this->shortOption == option[0];
+}
+
+Terminal::CommandText::~CommandText()
+{
+    shortOption = NULL;
+    longOption.clear();
+    text.clear();
+}
+
 Terminal::Terminal()
 {
     commands = new std::vector<std::string>{"add", "ext"};
+    commandsWithText = {
+        {"add", {
+                    {'h', "help", "add [options] <new-value>\n\tTo add the value to the table"},
+                }},
+        {"ext", {
+                    {'h', "help", "ext\n\tTo exit the program"},
+                }}};
 }
 
 Terminal::~Terminal()
 {
     delete[] commands;
+}
+
+std::string Terminal::getCommandText(std::vector<CommandText> &options, std::string option)
+{
+    for (CommandText commandText : options)
+    {
+        if (commandText.compare(option))
+        {
+            return commandText.text;
+        }
+    }
+
+    return "";
 }
 
 int Terminal::invoke(ILine *line)
@@ -17,6 +56,14 @@ int Terminal::invoke(ILine *line)
     std::vector<char> *shortOptions = line->getShortOptions();
     std::vector<std::string> *longOptions = line->getLongOptions();
     std::vector<std::string> *body = line->getBody();
+
+    for (int i = 0; i < longOptions->size(); i++)
+    {
+        std::string commandText = getCommandText(commandsWithText[invoker], longOptions->at(i));
+        if (!commandText.empty())
+        {
+        }
+    }
 
     if (invoker == "add")
     {
--- lab_itiop4/src/commander/Terminal.hpp
+++ lab_itiop4/src/commander/Terminal.hpp
@@ -2,12 +2,28 @@
 #define TERMINAL_HPP
 #include "../../include/commander/ITerminal.hpp"
 #include "../table/Table.hpp"
+#include <map>
 
 class Terminal : public ITerminal
 {
 private:
+    struct CommandText
+    {
+        char shortOption;
+        std::string longOption;
+        std::string text;
+
+        CommandText(char shortOption, std::string longOption, std::string text);
+        ~CommandText();
+
+        bool compare(std::string option);
+    } typedef CommandText;
+
     std::vector<std::string> *commands;
+    std::map<std::string, std::vector<CommandText>> commandsWithText;
     Table *table;
+
+    std::string getCommandText(std::vector<CommandText> &options, std::string option);
 
 public:
     Terminal();
