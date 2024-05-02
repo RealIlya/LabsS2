b1889df5c1888289963ea35f2b92c9167aa46c44
Denis V <denisveselyj8@gmail.com>
2024-05-02 21:23:04 +0700

    tvp4

---
+++ lab_tvp4/russian.h
@@ -0,0 +1,13 @@
+#ifndef _RUSSIAN_H
+#define _RUSSIAN_H
+
+#define число long double
+#define условие bool
+#define запускающая_функция int main()
+#define ввод std::cin
+#define вывод std::cout
+#define конец_строки std::endl
+#define возврат return
+#define самость int
+
+#endif
